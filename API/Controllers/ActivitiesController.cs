509da33f6a53640b82c191fe48e2240ee8457c80
Bass-Ninja <ninaa.emersic@gmail.com>
2023-01-17 07:52:03 +0100

    Add MediatR to Activities Controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,32 +3,31 @@ using Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
+        private readonly IMediator _mediator;
    
-        private readonly DataContext _context;
-   
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-           
-            
-        }
+       public ActivitiesController(IMediator mediator)
+       {
+            _mediator = mediator;
+        
+       }
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
 
         }
 
         [HttpGet("{id}")] //api/activities
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
-
+            return Ok();
         }
     }
 }
