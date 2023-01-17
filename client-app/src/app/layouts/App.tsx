2992215f7991046488c1a2c6a17b8fa6e3709644
Nina Emeršič <nina.emersic@inova.si>
2023-01-17 15:09:27 +0100

    Client side start

---
+++ client-app/src/app/layouts/App.tsx
@@ -0,0 +1,33 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([])
+
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5001/api/activities') 
+    .then(response => {
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <>
+        <NavBar/>
+        <Container style={{ marginTop: "7em" }}>
+          <ActivityDashboard activities={activities}/>
+        </Container>
+        
+    </>
+  );
+}
+
+export default App;
---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Button, Card, Grid, Item, ItemContent, Label, List, Segment } from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity';
+
+interface Props {
+    activities: Activity[];
+  }
+
+export default function ActivityList(props: Props) {
+    return (
+        <Segment>
+                <Item.Group divided>
+                    {props.activities.map(activity => (
+                        <Item key={activity.id}>
+                            <Item.Content>
+                                <Item.Header as="a">
+                                    {activity.title}
+                                </Item.Header>
+                                <Item.Meta>
+                                    {activity.date}
+                                </Item.Meta>
+                                <Item.Description>
+                                    <div>{activity.description}</div>
+                                    <div>{activity.city}, {activity.venue}</div>
+                                </Item.Description>
+                                <Item.Extra>
+                                    <Button floated="right" content="View" color="blue"/>
+                                    <Label basic content={activity.category}/>
+                                </Item.Extra>
+                            </Item.Content>
+                            
+                        </Item>
+                    ))}
+                </Item.Group>
+                
+        </Segment>
+        
+
+    )
+
+}
